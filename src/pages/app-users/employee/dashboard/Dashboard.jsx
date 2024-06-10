3f7716473b8f5a1780a1eb4b4a153254333c4baf
Diksha-Sapkal <dikshasapkal10@gmail.com>
2024-06-10 20:43:22 +0530

    Initial commit

--- src/pages/app-users/employee/dashboard/Dashboard.jsx
+++ src/pages/app-users/employee/dashboard/Dashboard.jsx
@@ -1,14 +1,12 @@
+import React from 'react';
 import { Helmet } from 'react-helmet-async';
-
 import DashboardPage from './view/DashboardPage';
-
-// ----------------------------------------------------------------------
 
 export default function AppPage() {
   return (
     <>
       <Helmet>
-        <title> Dashboard | Iloma </title>
+        <title> Dashboard | Nc-Tech </title>
       </Helmet>
 
       <DashboardPage />
