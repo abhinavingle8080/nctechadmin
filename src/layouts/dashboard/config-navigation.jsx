d7fadf12b0e809db1341582132920912cbf0e79e
Sagar <wakekarsagar397@gmail.com>
2024-06-12 16:58:03 +0530

    change sections on navbar

--- src/layouts/dashboard/config-navigation.jsx
+++ src/layouts/dashboard/config-navigation.jsx
@@ -9,24 +9,15 @@ const icon = (name) => (
 const navConfig = [
   {
     title: 'dashboard',         // Title for dashboard
-    path: '/user/dashboard',    // Path for dashboard
+    path: '/admin/dashboard',    // Path for dashboard
     icon: icon('dashboard'),    // Icon for dashboard
   },
-  {
-    title: 'Employee',          // Title for Employee section
-    path: '/employees',         // Path for Employee section
-    icon: icon('ic_user'),      // Icon for Employee section
-  },
+ 
 {
   title: 'Students',
   path: '/admin/students',
   icon: icon('ic_user'),
 },
-// {
-//   title: 'Batches',
-//   path: '/admin/teachers',
-//   icon: icon('ic_user'),    
-// },
 {
   title: 'Courses',
   path: '/admin/courses',
@@ -37,74 +28,6 @@ const navConfig = [
   path: '/admin/payments',
   icon: icon('ic_user'),
 },
-// {
-//   title: 'Holiday',
-//   path: '/admin/holidays',
-//   icon: icon('holiday'),
-// },
-// {
-//   title: 'Admission Sections',
-//   path: '/admin/subjects',
-//   icon: icon('ic_user'),
-// },
-
-  
-  // {
-  //   title: 'Leave',
-  //   path: '/admin/leaves',
-  //   icon: icon('ic_blog'),
-  // },
-//  {
-//     title: 'Designation',
-//     path: '/admin/designations',
-//     icon: icon('ic_blog'),
-//   },
-  // {
-  //   title: 'Notices',
-  //   path: '/admin/notices',
-  //   icon: icon('ic_blog'),
-  // },
-  // {
-  //   title: 'Teams',
-  //   path: '/admin/teams',
-  //   icon: icon('ic_blog'),
-  // },
-  
-  // {
-  //   title: 'login',
-  //   path: '/login',
-  //   icon: icon('ic_lock'),
-  // },
-  // {
-  //   title: 'Not found',
-  //   path: '/404',
-  //   icon: icon('ic_disabled'),
-  // },
-  {
-    title: 'Students',  // Title for Students section
-    path: '/students',  // Path for Students section
-    icon: icon('ic_user'),  // Icon for Students section
-  },
-  {
-    title: 'Batches',  // Title for Batches section
-    path: '/batches',  // Path for Batches section
-    icon: icon('ic_batches'),  // Icon for Batches section
-  },
-  {
-    title: 'Courses',  // Title for Courses section
-    path: '/courses',  // Path for Courses section
-    icon: icon('ic_courses'),  // Icon for Courses section
-  },
-  {
-    title: 'Holiday',  // Title for Holiday section
-    path: '/holidays',  // Path for Holiday section
-    icon: icon('ic_holiday'),  // Icon for Holiday section
-  },
-  {
-    title: 'Admission',  // Title for Admission section
-    path: '/admission',  // Path for Admission section
-    icon: icon('ic_admission'),  // Icon for Admission section
-  },
 ];
 
 export default navConfig;
