e31de366386fcbaad1c16388ce9de4943cecd44c
Sagarwakekar <wakekarsagar397@gmail.com>
2024-06-24 19:54:45 +0530

    changes in view payment

--- src/pages/app-users/admin/payments/view/PaymentDetails.jsx
+++ src/pages/app-users/admin/payments/view/PaymentDetails.jsx
@@ -1,17 +1,9 @@
 import React from 'react';
-import {Box, Grid, Typography } from '@mui/material';
+import { Box, Grid, Divider, Typography } from '@mui/material'; // Add Divider to the import statement
 import PropTypes from 'prop-types';
 import moment from 'moment';
 
 const styles = {
-  gridItem: {
-    borderBottom: '1px solid #ddd',
-    paddingTop: '8px',
-    paddingBottom: '8px',
-  },
-  noBorderBottom: {
-    borderBottom: 'none',
-  },
   container: {
     border: '1px solid #ddd',
     borderRadius: '8px',
@@ -22,9 +14,7 @@ const styles = {
     backgroundColor: '#f9f9f9',
   },
   header: {
-    borderBottom: '1px solid #ddd',
     marginBottom: '24px',
-    paddingBottom: '16px',
     textAlign: 'center',
   },
   section: {
@@ -32,11 +22,25 @@ const styles = {
   },
   sectionTitle: {
     fontWeight: 'bold',
-    marginBottom: '16px',
+    marginBottom: '8px',
   },
-  totalAmount: {
+  gridItem: {
+    borderBottom: '1px solid #ddd',
+    paddingTop: '8px',
+    paddingBottom: '8px',
+  },
+  noBorderBottom: {
+    borderBottom: 'none',
+  },
+  tableHeader: {
+    backgroundColor: '#f0f0f0',
     fontWeight: 'bold',
-    fontSize: '18px',
+  },
+  tableRow: {
+    borderBottom: '1px solid #ddd',
+  },
+  tableCell: {
+    padding: '8px',
   },
 };
 
@@ -46,46 +50,165 @@ PaymentDetails.propTypes = {
 
 function PaymentDetails({ data }) {
   const details = [
-    { label: 'Invoice Number ', value: data?.invoice_number || 'N/A' },
-    { label: 'Student Name  ', value: data?.Student ? `${data?.Student?.first_name} ${data?.Student?.last_name}` : 'N/A' },
-    { label: 'Course  ', value: data?.Course ? data?.Course?.course_name : 'N/A' },
-    { label: 'Payment Method  ', value: data?.payment_method || 'N/A' },
-    { label: 'Total Fees Amount  ', value: data?.Course ? `Rs.${data?.Course?.fees}` : 'N/A' },
-    { label: 'Discount Amount  ', value: data?.Course ? `Rs.${data?.Course?.discount_fees}` : 'N/A' },
-    { label: 'Paid Amount  ', value: data?.paid_amount ? `Rs.${data?.paid_amount}` : 'N/A' },
-    { label: 'Due Amount  ', value: data?.due_amount ? `Rs.${data?.due_amount}` : 'N/A' },
-    { label: 'Payment Date  ', value: data?.payment_date ? moment(data?.payment_date).format('DD/MM/YYYY') : 'N/A' },
-    { label: 'Payment Status ', value : data?.payment_status || 'N/A' },
+    { label: 'Invoice Number', value: data?.invoice_number || 'N/A' },
+    {
+      label: 'Student Name',
+      value: data?.Student ? `${data?.Student?.first_name} ${data?.Student?.last_name}` : 'N/A',
+    },
+    { label: 'Course', value: data?.Course ? data?.Course?.course_name : 'N/A' },
+    { label: 'Payment Method', value: data?.payment_method || 'N/A' },
+    { label: 'Total Fees Amount', value: data?.Course ? `Rs.${data?.Course?.fees}` : 'N/A' },
+    { label: 'Discount Amount', value: data?.Course ? `Rs.${data?.Course?.discount_fees}` : 'N/A' },
+    { label: 'Paid Amount', value: data?.paid_amount ? `Rs.${data?.paid_amount}` : 'N/A' },
+    { label: 'Due Amount', value: data?.due_amount ? `Rs.${data?.due_amount}` : 'N/A' },
+    {
+      label: 'Payment Date',
+      value: data?.payment_date ? moment(data?.payment_date).format('DD/MM/YYYY') : 'N/A',
+    },
+    { label: 'Payment Status', value: data?.payment_status || 'N/A' },
   ];
 
   return (
     <Box style={styles.container}>
       <Box style={styles.header}>
-        <Typography variant="h4">Payment Details</Typography>
+        <Typography variant="h4">INVOICE</Typography>
       </Box>
+
       <Grid container spacing={2}>
-        {details.map((item, index) => (
-          <Grid
-            item
-            xs={12}
-            md={6}
-            style={{
-              ...styles.gridItem,
-              ...(index === details.length - 1 ? styles.noBorderBottom : {}),
-            }}
-            key={index}
-          >
-            <Grid container>
-              <Grid item xs={12} md={4}>
-                <Typography variant="subtitle1" style={styles.sectionTitle}>{item.label}</Typography>
-              </Grid>
-              <Grid item xs={12} md={8}>
-                <Typography variant="body1">{item.value}</Typography>
-              </Grid>
+        <Grid item xs={12} md={6}>
+          <Typography variant="h5" style={styles.sectionTitle}>
+            Company Name
+          </Typography>
+          <Typography variant="body1">Non Criterion Technology Buldhana</Typography>
+          <Typography variant="body1">Saraswati Apartment Circular Road Buldhana </Typography>
+          <Typography variant="body1">Maharashtra 443001</Typography>
+          <Typography variant="body1">Phone : 9309393108</Typography>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <Typography variant="body1" style={styles.sectionTitle}>
+                INVOICE{' '}
+              </Typography>
+              <Typography variant="body1">{data?.invoice_number || 'N/A'}</Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant="body1" style={styles.sectionTitle}>
+                DATE
+              </Typography>
+              <Typography variant="body1">
+                {data?.payment_date ? moment(data?.payment_date).format('DD/MM/YYYY') : 'N/A'}
+              </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              {/* <Typography variant="body1" style={styles.sectionTitle}>Student ID</Typography>
+              <Typography variant="body1">{data?.id || 'N/A'}</Typography> */}
+            </Grid>
+            <Grid item xs={6}>
+              {/* <Typography variant="body1" style={styles.sectionTitle}>TERMS</Typography>
+              <Typography variant="body1">Due Upon Receipt</Typography> */}
             </Grid>
           </Grid>
-        ))}
+        </Grid>
       </Grid>
+
+      <Divider style={{ margin: '24px 0' }} />
+
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <Typography variant="h6" style={styles.sectionTitle}>
+            BILL TO
+          </Typography>
+          <Typography variant="body1">
+            {data?.Student ? `${data?.Student?.first_name} ${data?.Student?.last_name}` : 'N/A'}
+          </Typography>
+          <Typography variant="body1">{data?.Student?.address || 'N/A'}</Typography>
+          {/* <Typography variant="body1">Course : {data?.Course?.course_name || 'N/A'}</Typography> */}
+          {/* <Typography variant="body1">[City, ST ZIP]</Typography> */}
+          <Typography variant="body1">{data?.Student?.phone_no || 'N/A'}</Typography>
+          <Typography variant="body1">{data?.Student?.email || 'N/A'}</Typography>
+        </Grid>
+      </Grid>
+
+      <Divider style={{ margin: '24px 0' }} />
+
+      <Box>
+        <Grid container style={styles.tableHeader}>
+          <Grid item xs={6} style={styles.tableCell}>
+            <Typography variant="h6">COURSE</Typography>
+          </Grid>
+          <Grid item xs={2} style={styles.tableCell}>
+            <Typography variant="h6">TYPE</Typography>
+          </Grid>
+          <Grid item xs={2} style={styles.tableCell}>
+            {/* <Typography variant="body1">PAID AMOUNT</Typography> */}
+          </Grid>
+          <Grid item xs={2} style={styles.tableCell}>
+            <Typography variant="h6">AMOUNT</Typography>
+          </Grid>
+        </Grid>
+        <Grid container style={styles.tableRow}>
+          <Grid item xs={6} style={styles.tableCell}>
+            <Typography variant="body1">
+              {data?.Course?.course_name || 'N/A'} ({data?.Course?.type}){' '}
+            </Typography>
+          </Grid>
+          <Grid item xs={2} style={styles.tableCell}>
+            {/* <Typography variant="body1">{data?.Course?.type || 'N/A'}</Typography> */}
+          </Grid>
+          <Grid item xs={2} style={styles.tableCell}>
+            {/* <Typography variant="body1">{data?.paid_amount || 'N/A'}</Typography> */}
+          </Grid>
+          <Grid item xs={2} style={styles.tableCell}>
+            <Typography variant="body1">{data?.Course?.discount_fees || 'N/A'}</Typography>
+          </Grid>
+        </Grid>
+        <Grid container style={styles.tableRow}>
+          <Grid item xs={6} style={styles.tableCell}>
+            <Typography variant="body1">Paid Amount</Typography>
+          </Grid>
+          <Grid item xs={2} style={styles.tableCell}>
+            <Typography variant="body1">{data?.payment_method || 'N/A'}</Typography>
+          </Grid>
+          <Grid item xs={2} style={styles.tableCell}>
+            {/* <Typography variant="body1">-</Typography> */}
+          </Grid>
+          <Grid item xs={2} style={styles.tableCell}>
+            <Typography variant="body1">{data?.paid_amount || 'N/A'}</Typography>
+          </Grid>
+        </Grid>
+        <Grid container style={styles.tableRow}>
+          <Grid item xs={6} style={styles.tableCell}>
+            <Typography variant="body1">Remaining Amount</Typography>
+          </Grid>
+          <Grid item xs={2} style={styles.tableCell}>
+            <Typography variant="body1" />
+          </Grid>
+          <Grid item xs={2} style={styles.tableCell}>
+            <Typography variant="body1" />
+          </Grid>
+          <Grid item xs={2} style={styles.tableCell}>
+            <Typography variant="body1">
+              {data?.Course?.discount_fees !== undefined && data?.paid_amount !== undefined
+                ? (parseFloat(data.Course.discount_fees) - parseFloat(data.paid_amount)).toFixed(2)
+                : 'N/A'}
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid container>
+          <Grid item xs={6} />
+          <Grid item xs={4} style={styles.tableCell}>
+            <Typography variant="h6" style={styles.totalAmount}>
+              TOTAL PAID AMOUNT
+            </Typography>
+          </Grid>
+          <Grid item xs={2} style={styles.tableCell}>
+            <Typography variant="h6" style={styles.totalAmount}>
+              {data?.paid_amount || 'N/A'}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
     </Box>
   );
 }
